67a9481621c3c4064e8035b3f870cd4a449d4cf8
carwyn <carwyncollinsworth@gmail.com>
2025-03-24 21:13:48 -0700

    Mock website design

---
+++ script.js
@@ -0,0 +1,5 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // Add a simple welcome message to the console
+    console.log('Welcome to My Portfolio!');
+}); 
